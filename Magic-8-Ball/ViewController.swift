8370c4f33c1969037b9bbab0fe6e0b3823b3135c
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-04 15:06:50 -0700

    added segment controller

--- Magic-8-Ball/ViewController.swift
+++ Magic-8-Ball/ViewController.swift
@@ -8,8 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    var answers: [String] = []
+    
+    let magicBall = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    
+    let test = ["hello", "world"]
+    
+    let coinFlip = ["heads", "tails"]
 
+    @IBOutlet weak var segment: UISegmentedControl!
+    
     @IBOutlet weak var label: UILabel!
     
 
@@ -24,9 +32,20 @@ class ViewController: UIViewController {
     }
     
     func generateAnswer() {
+        print(segment.selectedSegmentIndex)
+        
+        if segment.selectedSegmentIndex == 0 {
+            answers = magicBall
+        } else if segment.selectedSegmentIndex == 1 {
+            answers = test
+        } else if segment.selectedSegmentIndex == 2 {
+            answers = coinFlip
+        }
+        
         let randomIndex = Int.random(in: 0..<answers.count)
-
+        
         label.text = answers[randomIndex]
+
     }
     
     override func viewDidLoad() {
