fe79170e26f9cc18f015a9a43a14a825698befc1
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-04 13:21:38 -0700

    init commit

---
+++ Magic-8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball
+//
+//  Created by Andrew Alsing on 4/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
